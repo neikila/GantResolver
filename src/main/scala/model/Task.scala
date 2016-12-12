123f023722bc38949f44e321c2fad23daf46a81c
k.neyman <k.neyman@corp.mail.ru>
2016-12-12 15:13:28 +0300

    Refactor Task

--- src/main/scala/model/Task.scala
+++ src/main/scala/model/Task.scala
@@ -3,25 +3,9 @@ package model
 /**
   * Created by k.neyman on 10.12.2016.
   */
-class Task(val start: Option[Int], val end: Option[Int], val timeBounds: TaskWithTimeBound) {
-  def this(task: TaskWithTimeBound) {
-    this(None, None, task)
-  }
-
+class Task(val start: Option[Int], val end: Option[Int], timeBounds: TaskWithTimeBound) extends
+  TaskWithTimeBound(timeBounds, timeBounds.startMin, timeBounds.endMax) {
   override def toString: String = s"Task #$id. Time: ($start, $end)"
-
-  def id = timeBounds.id
-  def length = timeBounds.length
-
-  def startMin = timeBounds.startMin
-  def endMin = timeBounds.endMin
-
-  def startMax = timeBounds.startMax
-  def endMax = timeBounds.endMax
-
-  def isCritical = timeBounds.isCritical
-
-  def taskType = timeBounds.taskType
 }
 
 object Task {
